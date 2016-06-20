3d7e60d04d6c9e9b47f37e399b79fec58ac3f924
yuhan <yud039@ucsd.edu>
2016-06-20 11:25:09 -0700

    Initial Commit

---
+++ SmartEV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartEV
+//
+//  Created by Yuhan Dai on 6/20/16.
+//  Copyright © 2016 Yuhan Dai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
