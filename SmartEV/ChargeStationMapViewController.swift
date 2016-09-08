4b15af52281608f852b325e5c1dac2b161182bba
yuhan dai <yud039@ucsd.edu>
2016-09-07 21:25:18 -0700

    added search bar and zoom in on user’s location

--- SmartEV/ChargeStationMapViewController.swift
+++ SmartEV/ChargeStationMapViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ChargeStationMapViewController: UIViewController {
+class ChargeStationMapViewController: UIViewController, CLLocationManagerDelegate {
     
     // MARK: Constants
-    // set initial location in Honolulu
-    let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
-    let regionRadius: CLLocationDistance = 1000
+    let locationManager = CLLocationManager()
+    
+    var resultSearchController:UISearchController? = nil
+    
     // MARK: Outlets
     
     @IBOutlet weak var mapView: MKMapView!
@@ -22,18 +24,53 @@ class ChargeStationMapViewController: UIViewController {
     // MARK: UIViewController Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.hideKeyboardWhenTappedAround()
         self.navigationController?.navigationBarHidden = false
         
-        centerMapOnLocation(initialLocation)
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.requestLocation()
+        }
+        
+        // show current location as a blue dot
+        self.mapView.showsUserLocation = true
+        
+        // embed search table into the map view
+        let locationSearchTable = storyboard!.instantiateViewControllerWithIdentifier("LocationSearchTable") as! LocationSearchTableViewController
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        // embed search bar into navigation bar
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
     }
     
-    // MARK: Actions
-    
     // MARK: helper functions
-    func centerMapOnLocation(location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
-                                                                  regionRadius * 2.0, regionRadius * 2.0)
-        mapView.setRegion(coordinateRegion, animated: true)
+    
+    /**
+     * Functions that centers at current location at initial time
+     * with specific radius
+     */
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        addErrorAlert(error.localizedDescription, alertTitle: "Location Error")
     }
 }
---
+++ SmartEV/LocationSearchTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSearchTableViewController.swift
+//  SmartEV
+//
+//  Created by Yuhan Dai on 9/7/16.
+//  Copyright © 2016 Yuhan Dai. All rights reserved.
+//
+
+import UIKit
+class LocationSearchTableViewController : UITableViewController {
+    
+}
+
+extension LocationSearchTableViewController : UISearchResultsUpdating {
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+    }
+}
